2e008b13639c6ea539deeaf50983d80a692bc40d
Samir Gayatri <samirgupta19670@gmail.com>
2022-04-28 23:53:01 +0530

    create_components

---
+++ src/Components/Shop.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Shop = () => {
+  return (
+    <div className="my-5">
+      <h2>Deposit / Withdraw Money</h2>
+      <button className="btn btn-primary mx-2">-</button>
+      Update Balance
+      <button className="btn btn-primary mx-2">+</button>
+    </div>
+  );
+};
+
+export default Shop;
